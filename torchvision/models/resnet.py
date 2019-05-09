994bddf8a998e47c711ca91d3a63d481db59e760
qilei123 <qileimail123@gmail.com>
2019-05-09 16:55:15 -0400

    fda

--- torchvision/models/resnet.py
+++ torchvision/models/resnet.py
@@ -5,10 +5,10 @@ from .utils import load_state_dict_from_url
 __all__ = ['ResNet', 'resnet18', 'resnet34', 'resnet50', 'resnet101',
            'resnet152', 'resnext50_32x4d', 'resnext101_32x8d','resnet101_wide']
 
-large_size_input = False
-conv1_kernel_size = 7
-conv1_padding_size = 3
-conv1_stride_size = 2
+
+conv1_kernel_size = 15
+conv1_padding_size = 7
+conv1_stride_size = 5
 
 model_urls = {
     'resnet18': 'https://download.pytorch.org/models/resnet18-5c106cde.pth',
@@ -117,7 +117,7 @@ class ResNet(nn.Module):
 
     def __init__(self, block, layers, num_classes=1000, zero_init_residual=False,
                  groups=1, width_per_group=64, replace_stride_with_dilation=None,
-                 norm_layer=None):
+                 norm_layer=None,large_size_input = False):
         super(ResNet, self).__init__()
         if norm_layer is None:
             norm_layer = nn.BatchNorm2d
