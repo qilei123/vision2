31a582f08916b1af3315180c6711de80611ddad3
qilei123 <qileimail123@gmail.com>
2019-05-14 16:33:50 -0400

    fda

--- torchvision/datasets/folder.py
+++ torchvision/datasets/folder.py
@@ -137,6 +137,7 @@ class DatasetFolder(VisionDataset):
         """
     
         path, target = self.samples[index]
+        print(path)
         sample = self.loader(path)
         if self.transform is not None:
             sample = self.transform(sample)
