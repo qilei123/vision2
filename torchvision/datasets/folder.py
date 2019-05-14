5047db70b19878abfc0450fa5bca0c9648ccc72b
qilei123 <qileimail123@gmail.com>
2019-05-14 17:07:48 -0400

    fda

--- torchvision/datasets/folder.py
+++ torchvision/datasets/folder.py
@@ -157,7 +157,6 @@ def pil_loader(path):
     # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
     with open(path, 'rb') as f:
         img = Image.open(f)
-        print(f)
         return img.convert('RGB')
 
 
