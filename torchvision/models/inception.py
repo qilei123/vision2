9e958e7f8a2461c61a401a7a304207a4750350b7
qilei123 <qileimail123@gmail.com>
2019-06-14 19:50:45 -0400

    fda

--- torchvision/models/inception.py
+++ torchvision/models/inception.py
@@ -114,9 +114,9 @@ class Inception3(nn.Module):
         elif wide:
             self.Conv2d_1a_3x3 = BasicConv2d(3, 32, kernel_size=15, stride=5,padding = 7)
         elif with_heatmap:
-            self.Conv2d_1a_3x3_with_heatmap = BasicConv2d(7, 32, kernel_size=3, stride=2)
-        
-        self.Conv2d_1a_3x3 = BasicConv2d(3, 32, kernel_size=3, stride=2)
+            self.Conv2d_1a_3x3a = BasicConv2d(7, 32, kernel_size=3, stride=2)
+        else:
+            self.Conv2d_1a_3x3 = BasicConv2d(3, 32, kernel_size=3, stride=2)
         self.Conv2d_2a_3x3 = BasicConv2d(32, 32, kernel_size=3)
         self.Conv2d_2b_3x3 = BasicConv2d(32, 64, kernel_size=3, padding=1)
         self.Conv2d_3b_1x1 = BasicConv2d(64, 80, kernel_size=1)
@@ -162,7 +162,7 @@ class Inception3(nn.Module):
             x3 = self.Conv2d_1a_3x3c(x)
             x = x1+x2+x3
         elif self.with_heatmap:
-            self.Conv2d_1a_3x3_with_heatmap(x)
+            self.Conv2d_1a_3x3a(x)
         else:
             x = self.Conv2d_1a_3x3(x)
         # N x 32 x 149 x 149
