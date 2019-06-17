a025d7706a6821bc854bf321261c19a3c328adef
qilei123 <qileimail123@gmail.com>
2019-06-17 02:16:20 -0400

    fda

--- torchvision/datasets/folder.py
+++ torchvision/datasets/folder.py
@@ -282,9 +282,7 @@ class DatasetFolder(VisionDataset):
             #print(sample.size())
             sample = sample+sample*heatmap[0]+sample*heatmap[1]+sample*heatmap[2]+sample*heatmap[3]
             sample = sample/4
-            print(sample.size())
-            cv2.imwrite('test.jpg',np.uint8(torch.Tensor.cpu(sample).detach().numpy()*255))
-            cv2.waitKey(0)
+
             #sample = torch.cat((sample,heatmap),0)            
         return sample, target
 
