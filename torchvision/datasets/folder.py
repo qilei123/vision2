dfd13c8a962f396fbc0e51a4eeb11671e22c322d
qilei123 <qileimail123@gmail.com>
2019-06-17 01:01:11 -0400

    fda

--- torchvision/datasets/folder.py
+++ torchvision/datasets/folder.py
@@ -264,7 +264,7 @@ class DatasetFolder(VisionDataset):
             print(target)
             pass
         if self.with_heatmap:
-            heatmap = self.get_heatmap(path,self.input_size)
+            heatmap = self.get_heatmap(path,2000)
             heatmap = heatmap.type(sample.dtype)
             sample = torch.cat((sample,heatmap),0)
             #print(sample.size())
